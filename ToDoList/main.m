681272748958b21d45fdd2d10fa0a7260ed7b936
Valentina Armenise <valentina.armenise@gmail.com>
2014-01-31 12:03:10 +0100

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by valentina armenise on 31/01/2014.
+//  Copyright (c) 2014 valentina armenise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "valentinaAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([valentinaAppDelegate class]));
+    }
+}
---
+++ ToDoList/valentinaAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  valentinaAppDelegate.h
+//  ToDoList
+//
+//  Created by valentina armenise on 31/01/2014.
+//  Copyright (c) 2014 valentina armenise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface valentinaAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
